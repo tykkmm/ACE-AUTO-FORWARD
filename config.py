9b4b7a2a6b00d6fe100a571d473655e7abc44deb
tykkmm <tykhanew45@gmail.com>
2024-07-23 06:56:38 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("OWNER")
+    API_ID = int(os.environ.get("29426486"))
+    API_HASH = os.environ.get("d71ad4ec048ab41677a1a439b21ff0c9")
+    AUTH_USERS = os.environ.get("5976437467")
 
